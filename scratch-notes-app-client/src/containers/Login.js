c386c16ecfb1b6e41a2b7f11a49d803a72f7d68a
vrbarros <victor.barros@windowslive.com>
2020-06-06 20:25:26 -0300

    Add the session to the state

---
+++ scratch-notes-app-client/src/containers/Login.js
@@ -0,0 +1,56 @@
+import { Auth } from 'aws-amplify';
+import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import React, { useState } from 'react';
+
+import { useAppContext } from '../libs/contextLib';
+
+import './Login.css';
+
+export default function Login() {
+  const { userHasAuthenticated } = useAppContext();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      await Auth.signIn(email, password);
+      userHasAuthenticated(true);
+    } catch (e) {
+      alert(e.message);
+    }
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="email" bsSize="large">
+          <ControlLabel>Email</ControlLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+        </FormGroup>
+        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+          Login
+        </Button>
+      </form>
+    </div>
+  );
+}
