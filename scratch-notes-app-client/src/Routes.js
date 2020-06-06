7b583b9bac6ab148d97bd4ea592cfcb579b0d93c
vrbarros <victor.barros@windowslive.com>
2020-06-06 20:14:32 -0300

    Handle 404s

--- scratch-notes-app-client/src/Routes.js
+++ scratch-notes-app-client/src/Routes.js
@@ -2,12 +2,16 @@ import { Route, Switch } from 'react-router-dom';
 import React from 'react';
 
 import Home from './containers/Home';
+import NotFound from './containers/NotFound';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route>
+        <NotFound />
       </Route>
     </Switch>
   );
