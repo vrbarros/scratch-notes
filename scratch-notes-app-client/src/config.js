e38159595b2fbc5899d2e3e9120ea783e295a3bf
vrbarros <victor.barros@windowslive.com>
2020-06-06 20:17:50 -0300

    Configure AWS Amplify

---
+++ scratch-notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'us-east-1',
+    BUCKET: 'scratch-notes-app-uploads-bvj78apyot',
+  },
+  apiGateway: {
+    REGION: 'us-east-1',
+    URL: 'https://fpyo5byrl9.execute-api.us-east-1.amazonaws.com/prod',
+  },
+  cognito: {
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_3uY1QQRDE',
+    APP_CLIENT_ID: '5hi8pt6vroi82cbd3ofgmf94et',
+    IDENTITY_POOL_ID: 'us-east-1:881fa5a3-b87e-47df-a406-18747e670692',
+  },
+};
