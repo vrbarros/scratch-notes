8379c1655b7b79e0221ebc6ed63f427509da60c6
vrbarros <victor.barros@windowslive.com>
2020-06-06 20:06:38 -0300

    Update application to use react-router

--- scratch-notes-app-client/src/index.js
+++ scratch-notes-app-client/src/index.js
@@ -1,12 +1,17 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
+import App from './App';
+
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
