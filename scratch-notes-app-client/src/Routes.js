059d5ee3c9e398f1d482ba786c9dc7575387769f
vrbarros <victor.barros@windowslive.com>
2020-06-07 11:30:42 -0300

    Signup with AWS Cognito

--- scratch-notes-app-client/src/Routes.js
+++ scratch-notes-app-client/src/Routes.js
@@ -2,14 +2,18 @@ import { Route, Switch } from 'react-router-dom';
 import React from 'react';
 
 import Home from './containers/Home';
-import NotFound from './containers/NotFound';
 import Login from './containers/Login';
+import NotFound from './containers/NotFound';
+import Signup from './containers/Signup';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/login">
         <Login />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
       </Route>
       <Route exact path="/">
         <Home />
