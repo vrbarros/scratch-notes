3b7797fbe6c341ee7fdad86b79b3408a45fef8ab
vrbarros <victor.barros@windowslive.com>
2020-06-07 14:01:24 -0300

    Update home lander

--- scratch-notes-app-client/src/containers/Home.js
+++ scratch-notes-app-client/src/containers/Home.js
@@ -1,4 +1,5 @@
 import { API } from 'aws-amplify';
+import { Link } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import { PageHeader, ListGroup, ListGroupItem } from 'react-bootstrap';
 import React, { useState, useEffect } from 'react';
@@ -61,6 +62,14 @@ export default function Home() {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
